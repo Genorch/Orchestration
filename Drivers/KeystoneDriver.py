594d23aee8ed99024550dc2d0a704e2b8cf4e40c
Saeed Arezoumand <saeed.arezoumand@gmail.com>
2017-05-22 13:02:58 -0400

    driver utils created

--- Drivers/KeystoneDriver.py
+++ Drivers/KeystoneDriver.py
@@ -1,6 +1,5 @@
 import json
-import httplib
-import traceback
+
 
 class KeystoneClient(object):
 
@@ -20,24 +19,3 @@ class KeystoneClient(object):
                              '", "password": "'+password+'"}}}')
         return json.loads(ret)
 
-    def _HTTPCall(self, host, port, method, url, header, body=None):
-
-        res = None
-        try:
-            _conn = httplib.HTTPConnection(host, port)
-            header['Content-Type']= "application/json"
-            _conn.request(method, url, body=body, headers=header)
-            res = _conn.getresponse()
-            ret = res.read()
-        except:
-            traceback.print_exc()
-            pass
-        if res.status in (httplib.OK,
-                          httplib.CREATED,
-                          httplib.ACCEPTED,
-                          httplib.NO_CONTENT):
-            return ret
-
-        raise httplib.HTTPException(
-            res, 'code %d reason %s' % (res.status, res.reason),
-            res.getheaders(), res.read())
---
+++ Drivers/utils.py
@@ -0,0 +1,28 @@
+import httplib
+import traceback
+
+
+class Utils(object):
+
+    @staticmethod
+    def http_call(self, host, port, method, url, header, body=None):
+
+        res = None
+        try:
+            _conn = httplib.HTTPConnection(host, port)
+            header['Content-Type']= "application/json"
+            _conn.request(method, url, body=body, headers=header)
+            res = _conn.getresponse()
+            ret = res.read()
+        except:
+            traceback.print_exc()
+            pass
+        if res.status in (httplib.OK,
+                          httplib.CREATED,
+                          httplib.ACCEPTED,
+                          httplib.NO_CONTENT):
+            return ret
+
+        raise httplib.HTTPException(
+            res, 'code %d reason %s' % (res.status, res.reason),
+            res.getheaders(), res.read())
