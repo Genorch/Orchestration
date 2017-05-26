2c35611b8f4664c8263d29e8a12c1fa1f14413dc
Parham Alvani <parham.alvani@gmail.com>
2017-05-26 23:27:37 +0430

    Better error handling.

--- providers/openstack/nova_driver.py
+++ providers/openstack/nova_driver.py
@@ -5,19 +5,22 @@ from novaclient.client import Client
 
 class NovaDriver:
     def __init__(self):
-        with open('../config/os.json') as os_config_file:
-                os_cfg = json.load(os_config_file)
+        try:
+            os_cfg = json.load(open('../config/os.json'))
 
-                credentials = {
-                        "version": os_cfg['OS_COMPUTE_API_VERSION'],
-                        "auth_url": os_cfg['OS_AUTH_URL'],
-                        "username": os_cfg['OS_USERNAME'],
-                        "api_key": os_cfg['OS_PASSWORD'],
-                        "project_id": os_cfg['OS_PROJECT_NAME'],
-                        "region_name": os_cfg['OS_REGION_NAME']
-                        }
+            credentials = {
+                "version": os_cfg['OS_COMPUTE_API_VERSION'],
+                "auth_url": os_cfg['OS_AUTH_URL'],
+                "username": os_cfg['OS_USERNAME'],
+                "api_key": os_cfg['OS_PASSWORD'],
+                "project_id": os_cfg['OS_PROJECT_NAME'],
+                "region_name": os_cfg['OS_REGION_NAME']
+            }
 
-                self.nova = Client(**credentials)
+            self.nova = Client(**credentials)
+        except FileNotFoundError:
+            print("OpenStack configuration not found.")
+            exit()
 
     def get_vms(self):
         return self.nova.servers.list()
