57976777c0c2b7a4922b8d4a18b8ff92017de49b
Parham Alvani <parham.alvani@gmail.com>
2017-05-26 23:28:10 +0430

    Improves openstack provider.

--- providers/openstack/__init__.py
+++ providers/openstack/__init__.py
@@ -1,8 +1,13 @@
 from ..base import Provider
+from .nova_driver import NovaDriver
 
 
 class OpenStackProvider(Provider):
     name = 'openstack'
 
     def __init__(self):
+        self.nova_driver = NovaDriver()
+
+    def create_server(self):
+        print(self.get_vms())
         pass
