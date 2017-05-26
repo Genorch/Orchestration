06ec45d3564c404ef9b115c63118dd0c5029cb06
Parham Alvani <parham.alvani@gmail.com>
2017-05-26 12:37:26 +0430

    Server abstraction was improved.

--- domain/server.py
+++ domain/server.py
@@ -1,6 +1,8 @@
-from ..providers.base import BaseProvider
+from providers.base import BaseProvider
 
 
 class Server:
-    def __init__(self, provider):
+    def __init__(self, _id, _class, provider):
+        self._id = _id
+        self._class = _class
         self.provider = BaseProvider.get(provider)
