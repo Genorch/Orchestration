9568cb49b2c1ca91ffdb75605e3949909e9d1610
Parham Alvani <parham.alvani@gmail.com>
2017-05-26 12:15:01 +0430

    Genorch entry point for parsing yaml was added.

--- genorch-serve.py
+++ genorch-serve.py
@@ -3,13 +3,16 @@
 import yaml
 import click
 
+
 @click.command()
 @click.option('--load', prompt='YAML file',
               help='Target yaml file to parse')
 def parse(load):
     with open(load) as stream:
         m = yaml.load(stream)
-        click.echo_via_pager(m)
+        for server in m['project']['service']:
+            print(server)
+
 
 if __name__ == '__main__':
     parse()
