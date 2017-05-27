16e9a3c54480e98faeac76bc230ef0ae1b75835a
Iman Tabrizian <Tabrizian@outlook.com>
2017-05-27 05:05:35 +0430

    Improve ansible module

--- ansible/__init__.py
+++ ansible/__init__.py
@@ -1,15 +1,19 @@
+import json
+
 from ansible.playbook import PlayBook
 
 
 class AnsibleDriver:
-    def __init__(self, _class):
-        self.pb = PlayBook(
-                playbook='ansible/playbooks/%s.yml' % _class,
-                host_list=[],
-                remote_user='ubuntu',
-                private_key_file='/path/to/key.pem'
-        )
+    def __init__(self):
+        pass
 
-    def setup(self):
-        results = self.pb.run()
+    def setup(self, playbook):
+        ansible_cfg = json.load(open('../config/ansible.json'))
+        pb = PlayBook(
+                playbook='ansible/playbooks/%s.yml' % playbook,
+                host_list=[],
+                remote_user=ansible_cfg['REMOTE_USER'],
+                private_key_file=ansible_cfg['PRIVATE_KEY']
+        )
+        results = pb.run()
         return results
