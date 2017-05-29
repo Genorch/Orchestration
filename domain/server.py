d57b787935809a1e166a0d4d1467817ed87af07d
Parham Alvani <parham.alvani@gmail.com>
2017-05-30 02:15:54 +0430

    Server abstraction was completed.

--- domain/server.py
+++ domain/server.py
@@ -2,13 +2,12 @@ from providers.base import BaseProvider
 
 
 class Server:
-    def __init__(self, _id, resources, provider):
-        '''
-        :param resources: (ram, hard, vcpu)
-        '''
+    def __init__(self, _id, image, flavor, region, provider):
         self._id = _id
-        self.resources = resources
-        self.provider = BaseProvider.get(provider)()
+        self.flavor = flavor
+        self.region = region
+        self.image = image
+        self.provider = BaseProvider.get(provider)(region)
 
     def create(self):
-        self.provider.create_server(*self.resources)
+        self.provider.create_server(self.image, self.flavor, self._id)
