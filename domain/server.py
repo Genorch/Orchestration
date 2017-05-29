4d92a1ff808e8e1e03a250f84782a7c165012550
Parham Alvani <parham.alvani@gmail.com>
2017-05-30 00:01:14 +0430

    Improves server abstraction.

--- domain/server.py
+++ domain/server.py
@@ -2,10 +2,13 @@ from providers.base import BaseProvider
 
 
 class Server:
-    def __init__(self, _id, _class, provider):
+    def __init__(self, _id, resources, provider):
+        '''
+        :param resources: (ram, hard, vcpu)
+        '''
         self._id = _id
-        self._class = _class
+        self.resources = resources
         self.provider = BaseProvider.get(provider)()
 
     def create(self):
-        self.provider.create_server()
+        self.provider.create_server(*self.resources)
