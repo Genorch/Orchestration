d02514f26e1190287fbe0e8450c0fde00964bca5
Parham Alvani <parham.alvani@gmail.com>
2017-05-30 02:16:08 +0430

    Openstack service was improved.

--- providers/openstack/__init__.py
+++ providers/openstack/__init__.py
@@ -5,10 +5,9 @@ from .nova_driver import NovaDriver
 class OpenStackProvider(Provider):
     name = 'openstack'
 
-    def __init__(self):
-        self.nova_driver = NovaDriver()
+    def __init__(self, region):
+        self.nova_driver = NovaDriver(region)
 
-    def create_server(self, ram, vcpus, disk):
+    def create_server(self, image_name, flavor_name, instance_name):
         # TODO ansible initiation, image name and network configuration
-        flavor_id = self.create_flavor()
-        self.boot_vm(flavor_id)
+        self.nova_driver.boot_vm(image_name, flavor_name, [], instance_name)
