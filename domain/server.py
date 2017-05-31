6427b46e5c614d834ba68bc7ef628e401dd3747b
Parham Alvani <parham.alvani@gmail.com>
2017-05-31 23:31:35 +0430

    Server abstraction was improved.

--- domain/server.py
+++ domain/server.py
@@ -3,7 +3,8 @@ from providers.base import BaseProvider
 
 class Server:
     def __init__(self, _id, image, flavor, region, provider, networks):
-        self._id = _id
+        self._id = None
+        self.name = _id
         self.flavor = flavor
         self.region = region
         self.image = image
@@ -11,5 +12,10 @@ class Server:
         self.provider = BaseProvider.get(provider)(region)
 
     def create(self):
-        self.provider.create_server(
-                self.image, self.flavor, self._id, self.networks)
+        # TODO ansible initiation
+        self._id = self.provider.create_server(
+            self.image, self.flavor, self.name, self.networks)
+
+    @property
+    def ips(self):
+        print(self.provider.ips(self._id))
