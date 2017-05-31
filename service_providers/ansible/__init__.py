425931dd48c7a6a1104ef4b9d54029cd380471e5
Iman Tabrizian <Tabrizian@outlook.com>
2017-05-31 23:17:51 +0430

    Improves ansible module creation

--- service_providers/ansible/__init__.py
+++ service_providers/ansible/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 from collections import namedtuple
 
 from ansible.parsing.dataloader import DataLoader
@@ -11,11 +13,11 @@ from ansible.executor.playbook_executor import PlaybookExecutor
 
 from callback import ResultCallback
 
-Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'listhosts', 'syntax'])
+Options = namedtuple('Options', ['connection', 'forks', 'become', 'become_method', 'become_user', 'check', 'listhosts', 'syntax'])
 # initialize needed objects
 variable_manager = VariableManager()
 loader = DataLoader()
-options = Options(connection='local', module_path='/path/to/mymodules', forks=100, become=None, become_method=None, become_user=None, check=False, listhosts=True, syntax=False)
+options = Options(connection='local', forks=100, become=None, become_method=None, become_user=None, check=False, listhosts=True, syntax=False)
 passwords = dict(vault_pass='secret')
 
 # Instantiate our ResultCallback for handling results as they come in
@@ -30,9 +32,36 @@ variable_manager.set_inventory(inventory)
 # actually run it
 tqm = None
 try:
-    pbex = PlaybookExecutor(playbooks=["playbooks/apache.yml"], inventory=inventory, variable_manager=variable_manager, loader=loader, options=options, passwords=passwords)
+    pbex = PlaybookExecutor(
+            playbooks=["playbooks/apache.yml"],
+            inventory=inventory,
+            variable_manager=variable_manager,
+            loader=loader,
+            options=options,
+            passwords=passwords
+            )
 
     results = pbex.run()
+    for p in results:
+
+        for idx, play in enumerate(p['plays']):
+            if play._included_path is not None:
+                loader.set_basedir(play._included_path)
+            else:
+                pb_dir = os.path.realpath(os.path.dirname(p['playbook']))
+                loader.set_basedir(pb_dir)
+                msg = "\n  play #%d (%s): %s" % (idx + 1, ','.join(play.hosts), play.name)
+                mytags = set(play.tags)
+                msg += '\tTAGS: [%s]' % (','.join(mytags))
+
+                if options.listhosts:
+                    playhosts = set(inventory.get_hosts(play.hosts))
+                    msg += "\n    pattern: %s\n    hosts (%d):" % (play.hosts, len(playhosts))
+                    for host in playhosts:
+                        msg += "\n      %s" % host
+
+                print(msg)
+
 
 finally:
     if tqm is not None:
