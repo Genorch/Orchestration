967a72ef7390397b4bfbabf22ccbd23376d316fe
Iman Tabrizian <Tabrizian@outlook.com>
2017-05-31 22:30:11 +0430

    [WIP] Improves ansible driver

--- service_providers/ansible/__init__.py
+++ service_providers/ansible/__init__.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
 from collections import namedtuple
+
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars import VariableManager
 from ansible.inventory import Inventory
-from ansible.playbook.play import Play
-from ansible.executor.task_queue_manager import TaskQueueManager
+from ansible.playbook.play_context import PlayContext
+from ansible.executor.playbook_executor import PlaybookExecutor
 
-Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check'])
+
+from callback import ResultCallback
+
+Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'listhosts', 'syntax'])
 # initialize needed objects
 variable_manager = VariableManager()
 loader = DataLoader()
-options = Options(connection='local', module_path='/path/to/mymodules', forks=100, become=None, become_method=None, become_user=None, check=False)
+options = Options(connection='local', module_path='/path/to/mymodules', forks=100, become=None, become_method=None, become_user=None, check=False, listhosts=True, syntax=False)
 passwords = dict(vault_pass='secret')
 
 # Instantiate our ResultCallback for handling results as they come in
@@ -22,29 +26,14 @@ inventory = Inventory(loader=loader, variable_manager=variable_manager, host_lis
 variable_manager.set_inventory(inventory)
 
 # create play with tasks
-play_source =  dict(
-        name = "Ansible Play",
-        hosts = 'localhost',
-        gather_facts = 'no',
-        tasks = [
-            dict(action=dict(module='shell', args='ls'), register='shell_out'),
-            dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
-         ]
-    )
-play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
 
 # actually run it
 tqm = None
 try:
-    tqm = TaskQueueManager(
-              inventory=inventory,
-              variable_manager=variable_manager,
-              loader=loader,
-              options=options,
-              passwords=passwords,
-              stdout_callback=results_callback,  # Use our custom callback instead of the ``default`` callback plugin
-          )
-    result = tqm.run(play)
+    pbex = PlaybookExecutor(playbooks=["playbooks/apache.yml"], inventory=inventory, variable_manager=variable_manager, loader=loader, options=options, passwords=passwords)
+
+    results = pbex.run()
+
 finally:
     if tqm is not None:
         tqm.cleanup()
