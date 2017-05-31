7c6f4c43d5a55da28039a5f54b1c29ac15ef77cf
Iman Tabrizian <Tabrizian@outlook.com>
2017-05-31 21:46:52 +0430

    Create sample callback

---
+++ service_providers/ansible/callback.py
@@ -0,0 +1,9 @@
+import json
+
+from ansible.plugins.callback import CallbackBase
+
+
+class ResultCallback(CallbackBase):
+    def v2_runner_on_ok(self, result, **kwargs):
+        host = result._host
+        print(json.dumps({host.name: result._result}, indent=4))
