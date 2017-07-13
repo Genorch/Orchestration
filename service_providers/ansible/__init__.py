d857299bfd0887aa2e619e788d4559365c1f304a
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-13 14:51:13 +0430

    Improves ansible configuration

--- service_providers/ansible/__init__.py
+++ service_providers/ansible/__init__.py
@@ -1,68 +1,32 @@
 #!/usr/bin/env python
 
-import os
-
-from collections import namedtuple
-
-from ansible.parsing.dataloader import DataLoader
-from ansible.vars import VariableManager
-from ansible.inventory import Inventory
-from ansible.playbook.play_context import PlayContext
 from ansible.executor.playbook_executor import PlaybookExecutor
+from ansible.inventory import Inventory
+from ansible.vars import VariableManager
+from ansible.parsing.dataloader import DataLoader
 
 
-from callback import ResultCallback
+class Ansible:
 
-Options = namedtuple('Options', ['connection', 'forks', 'become', 'become_method', 'become_user', 'check', 'listhosts', 'syntax'])
-# initialize needed objects
-variable_manager = VariableManager()
-loader = DataLoader()
-options = Options(connection='local', forks=100, become=None, become_method=None, become_user=None, check=False, listhosts=True, syntax=False)
-passwords = dict(vault_pass='secret')
+    def __init__(self):
 
-# Instantiate our ResultCallback for handling results as they come in
-results_callback = ResultCallback()
+        self.variable_manager = VariableManager()
+        self.loader = DataLoader()
+        self.passwords = {}
+        self.inventory = Inventory(
+                loader=self.loader,
+                variable_manager=self.variable_manager,
+                host_list=self.options.inventory
+                )
 
-# create inventory and pass to var manager
-inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list='localhost')
-variable_manager.set_inventory(inventory)
+        self.variable_manager.set_inventory(self.inventory)
 
-# create play with tasks
-
-# actually run it
-tqm = None
-try:
-    pbex = PlaybookExecutor(
-            playbooks=["playbooks/apache.yml"],
-            inventory=inventory,
-            variable_manager=variable_manager,
-            loader=loader,
-            options=options,
-            passwords=passwords
-            )
-
-    results = pbex.run()
-    for p in results:
-
-        for idx, play in enumerate(p['plays']):
-            if play._included_path is not None:
-                loader.set_basedir(play._included_path)
-            else:
-                pb_dir = os.path.realpath(os.path.dirname(p['playbook']))
-                loader.set_basedir(pb_dir)
-                msg = "\n  play #%d (%s): %s" % (idx + 1, ','.join(play.hosts), play.name)
-                mytags = set(play.tags)
-                msg += '\tTAGS: [%s]' % (','.join(mytags))
-
-                if options.listhosts:
-                    playhosts = set(inventory.get_hosts(play.hosts))
-                    msg += "\n    pattern: %s\n    hosts (%d):" % (play.hosts, len(playhosts))
-                    for host in playhosts:
-                        msg += "\n      %s" % host
-
-                print(msg)
-
-
-finally:
-    if tqm is not None:
-        tqm.cleanup()
+    def execute(self):
+        pbex = PlaybookExecutor(
+                playbooks=['playbooks/apache.yml'],
+                inventory=self.inventory,
+                variable_manager=self.variable_manager,
+                loader=self.loader,
+                options="", passwords=self.passwords
+                )
+        pbex.run()
