18a08f4940a1dc8a1fa704767599d4f6fefdaeb3
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-14 12:59:40 +0430

    Adds initial configuration manager

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+import configparser
+
+
+class Config:
+
+    def __init__(self):
+        self.parser = configparser.ConfigParser()
+        self.parser.read('./ansible.ini')
+
+    def __getattr__(self, name):
+        return self.parser['DEFAULT'][name]
