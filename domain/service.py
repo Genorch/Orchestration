3d10df034701ad07de28dfe9465cd44fe13c49e8
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-16 22:34:18 +0430

    Refactors ip addresses from network interfaces

--- domain/service.py
+++ domain/service.py
@@ -1,11 +1,21 @@
 from service_providers.ansible_driver import Ansible
+from database import db
+from tinydb import where
 
 
 class Service:
-    def __init__(self, name, targets, provider):
+    def __init__(self, name, targets, provider=Ansible()):
         self.name = name
-        self.targets = targets
-        self.provider = Ansible()
+
+        ips = []
+        for vm in targets:
+            network_intefaces = db.vms.search(where('name') == vm)[0]['ips']
+            print(network_intefaces)
+            for nic in network_intefaces:
+                ips.append(network_intefaces[nic][0]['addr'])
+
+        self.targets = ips
+        self.provider = provider
 
     def create(self):
         self.provider.create_service(self.name, self.targets)
