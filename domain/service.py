a83e8fc64f566431974051cc603db8295bf07364
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-16 11:07:35 +0430

    Adds service domain

---
+++ domain/service.py
@@ -0,0 +1,11 @@
+from service_providers.ansible_driver import Ansible
+
+
+class Service:
+    def __init__(self, name, targets, provider):
+        self.name = name
+        self.targets = targets
+        self.provider = Ansible()
+
+    def create_service(self):
+        self.provider.create_service(self.name, self.targets)
