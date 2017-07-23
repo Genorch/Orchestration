297707f5e0fd0e28dd1207bd697322ce9c830b72
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-23 12:25:34 +0430

    Improves ansible driver

--- service_providers/ansible/__init__.py
+++ service_providers/ansible/__init__.py
@@ -15,29 +15,31 @@ class Ansible(ServiceProvider):
 
     def __init__(self, targets):
 
+        ansible_cfg = cfg['ansible']
+
         self.variable_manager = VariableManager()
         self.loader = DataLoader()
         self.passwords = {}
         self.options = Options()
-        self.options.become_method = cfg.BECOME_METHOD
-        self.options.become_user = cfg.BECOME_USER
+        self.options.become_method = ansible_cfg['BECOME_METHOD']
+        self.options.become_user = ansible_cfg['BECOME_USER']
         self.options.become = True
-        self.options.private_key_file = cfg.PRIVATE_SSH_KEY
-        self.options.connection = cfg.CONNECTION
+        self.options.private_key_file = ansible_cfg['PRIVATE_SSH_KEY']
+        self.options.connection = ansible_cfg['CONNECTION']
 
         self.inventory = Inventory(
                 loader=self.loader,
                 variable_manager=self.variable_manager,
                 host_list=targets
                 )
-        self.passwords = {'become_pass': cfg.BECOME_PASS}
+        self.passwords = {'become_pass': ansible_cfg['BECOME_PASS']}
 
         self.options.hostlist = targets
         self.variable_manager.set_inventory(self.inventory)
 
     def create_service(self, opts):
         pbex = PlaybookExecutor(
-                playbooks=['service_providers/ansible_driver/playbooks/' +
+                playbooks=['service_providers/ansible/playbooks/' +
                            opts['playbook']],
                 inventory=self.inventory,
                 variable_manager=self.variable_manager,
