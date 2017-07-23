87b59cbeb9d7f52cca7f38a749d52350bb0e424e
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-23 12:15:28 +0430

    Improves the service model

--- domain/service.py
+++ domain/service.py
@@ -1,21 +1,20 @@
-from service_providers.ansible_driver import Ansible
 from database import db
 from tinydb import where
+from service_providers.base import BaseServiceProvider
 
 
 class Service:
-    def __init__(self, name, targets):
-        self.name = name
+    def __init__(self, provider, targets, opts):
+        self.opts = opts
 
         ips = []
         for vm in targets:
             network_interfaces = db.vms.search(where('name') == vm)[0]['ips']
-            print(network_interfaces)
             for nic in network_interfaces:
                 ips.append(network_interfaces[nic][0]['addr'])
 
         self.targets = ips
-        self.provider = Ansible(self.targets)
+        self.provider = BaseServiceProvider.get(provider)(targets)
 
     def create(self):
-        self.provider.create_service(self.name)
+        self.provider.create_service(self.opts)
