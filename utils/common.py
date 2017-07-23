64ae6a510f2aafe3ad38a1901d375f107fcc077b
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-23 18:19:03 +0430

    Adds id to ip translator

---
+++ utils/common.py
@@ -0,0 +1,10 @@
+from database import db
+from tinydb import where
+
+def translate_id(id):
+    ips = []
+    network_interfaces = db.vms.search(where('name') == id)[0]['ips']
+    for nic in network_interfaces:
+        ips.append(network_interfaces[nic][0]['addr'])
+
+    return ips
