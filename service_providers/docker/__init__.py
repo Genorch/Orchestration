d0147eb966780786f17bf433de1ace6f148e2a3f
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-31 10:07:45 +0430

    Adds swarm sub driver

--- service_providers/docker/__init__.py
+++ service_providers/docker/__init__.py
@@ -14,5 +14,19 @@ class Docker(ServiceProvider):
 
     def create_service(self, opts):
         for target in self.targets:
-            docker_client = docker.DockerClient("tcp://" + target + cfg.docker['API_PORT'])
-            docker_client.containers.run(**opts, detach=True)
+            docker_client = docker.DockerClient("tcp://" + target +
+                                                ":" + cfg.docker['API_PORT'])
+            if "sub_driver" in opts:
+                sub_driver_name = opts['sub_driver']
+                sub_driver = getattr(sub_driver_name, docker_client)
+
+                del opts['sub_driver']
+
+                if sub_driver_name == "swarm":
+                    if opts['manager']:
+                        sub_driver.init(listen_addr='0.0.0.0:5000')
+                    else:
+                        sub_driver.init(listen_addr='0.0.0.0:5000')
+            else:
+                docker_client.containers.run(**opts, detach=True)
+
