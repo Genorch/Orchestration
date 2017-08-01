0ef42c9f9b076c169672d81d683467c0a0ad0b83
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-01 14:51:20 -0400

    Fixes minor bugs in swarm

--- service_providers/docker/__init__.py
+++ service_providers/docker/__init__.py
@@ -17,6 +17,7 @@ def insert_join_token(tokens):
 
         element['docker']['join_tokens'] = tokens
         return element
+    return transform
 
 
 class Docker(ServiceProvider):
@@ -31,19 +32,21 @@ class Docker(ServiceProvider):
                                                 ':' + cfg.docker['API_PORT'])
             if "sub_driver" in opts:
                 sub_driver_name = opts['sub_driver']
-                sub_driver = getattr(sub_driver_name, docker_client)
+                sub_driver = getattr(docker_client, sub_driver_name)
 
                 del opts['sub_driver']
 
                 if sub_driver_name == 'swarm':
-                    sub_driver_opts = opts['sub_driver']['opts']
+                    sub_driver_opts = opts['opts']
                     node_type = sub_driver_opts['type']
 
-                    del sub_driver_opts['sub_driver']
-
                     if node_type == 'manager':
-                        sub_driver.init('0.0.0.0:' + cfg.docker['SWARM_PORT'])
+                        sub_driver.init('eth0:' + cfg.docker['SWARM_PORT'], '0.0.0.0:' + cfg.docker['SWARM_PORT'])
+                        print(sub_driver.attrs['JoinTokens'])
                         db.vms.update(insert_join_token(sub_driver.attrs['JoinTokens']), where('name') == target)
+                    elif node_type == 'worker':
+                        manager = db.vms.get(where('name') == sub_driver_opts['managers'][0])
+                        sub_driver.join([common.id_to_swarm(sub_driver_opts['managers'][0])], manager['docker']['join_tokens']['Worker'], '0.0.0.0:' + cfg.docker['SWARM_PORT'])
 
             else:
                 docker_client.containers.run(**opts, detach=True)
