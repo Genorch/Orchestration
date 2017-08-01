7748aff58386fa2bca9e3d7e5f9ae03e79569063
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-01 15:55:59 +0430

    Service model shouldn't translate id to ip

--- domain/service.py
+++ domain/service.py
@@ -1,16 +1,10 @@
 from service_providers.base import BaseServiceProvider
-from utils import common
 
 
 class Service:
     def __init__(self, provider, targets, opts):
         self.opts = opts
-
-        ips = []
-        for vm in targets:
-            ips.extend(common.translate_id(vm))
-
-        self.targets = ips
+        self.targets = targets
         self.provider = BaseServiceProvider.get(provider)(self.targets)
 
     def create(self):
