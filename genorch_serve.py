a7bd56b0f8a585009c81b632d0bfe767109cbfbd
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-01 13:48:41 +0430

    Improves parser

--- genorch_serve.py
+++ genorch_serve.py
@@ -2,7 +2,7 @@
 
 import yaml
 import click
-from jinja2 import Template, Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader
 
 from domain.server import Server
 from domain.service import Service
@@ -11,43 +11,48 @@ from utils.common import translate_id
 from database import db
 from tinydb import where
 
+
 @click.group()
 @click.version_option('0.0.1')
 def cli():
     pass
 
+
 @cli.command()
 @click.option('--load', prompt='YAML file',
               help='Target yaml file to parse')
 def parse(load):
-    with open(load) as stream:
-        env = Environment(
-            loader=FileSystemLoader('./')
-        )
-        env.globals.update(translate_id=translate_id)
+    env = Environment(
+        loader=FileSystemLoader('./')
+    )
+    env.globals.update(translate_id=translate_id)
 
-        m = env.get_template(load).render()
+    m = env.get_template(load).render()
 
-        m = yaml.load(m)
-        for provider in m['project']['topology']['provider']:
-            for region in provider['region']:
-                for vm in region['vm']:
-                    click.secho('virtual machine => provider: %s, region: %s' %
-                                (provider['name'], region['name']),
-                                fg="green")
+    m = yaml.load(m)
+    for provider in m['project']['topology']['provider']:
+        for region in provider['region']:
+            for vm in region['vm']:
+                click.secho('virtual machine => provider: %s, region: %s' %
+                            (provider['name'], region['name']),
+                            fg="green")
 
-                    servers = db.vms.search(where('name') == vm['id'])
+                servers = db.vms.search(where('name') == vm['id'])
 
-                    if len(servers) == 0:
-                        Server(vm['id'], vm['image'], vm['flavor'],
-                               region['name'], provider['name'],
-                               vm['networks'], vm.get('key', None)).create()
+                if len(servers) == 0:
+                    Server(vm['id'], vm['image'], vm['flavor'],
+                           region['name'], provider['name'],
+                           vm['networks'], vm.get('key', None)).create()
 
+    if m['project']['service']:
         for service in m['project']['service']:
             click.secho('service => provider: %s, type: %s' %
                         (service['provider'], service['type']),
                         fg="blue")
-            Service(service['provider'], service['targets'], service['opts']).create()
+            Service(
+                    service['provider'],
+                    service['targets'],
+                    service['opts']).create()
 
 
 @cli.command()
