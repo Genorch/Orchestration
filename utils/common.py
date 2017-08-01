e5a7ba28373afa7ad30611fd4e60a1e189c41db6
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-01 15:23:17 +0430

    Adds a new function

--- utils/common.py
+++ utils/common.py
@@ -1,5 +1,6 @@
 from database import db
 from tinydb import where
+from config import cfg
 
 
 def translate_id(id):
@@ -9,3 +10,9 @@ def translate_id(id):
         ips.append(network_interfaces[nic][0]['addr'])
 
     return ips
+
+
+def id_to_swarm(id):
+    ip = translate_id(id)[0]
+    return ip + ':' + cfg.docker['SWARM_PORT']
+
