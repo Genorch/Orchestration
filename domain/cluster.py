080d6f2ffd5cf61d01864080fb44bbe80a1d8625
Iman Tabrizian <Tabrizian@outlook.com>
2017-09-02 20:27:19 +0430

    Adds cluster blueprint

---
+++ domain/cluster.py
@@ -0,0 +1,31 @@
+from cluster_providers.base import BaseProvider
+from database import db
+from utils import common
+
+
+class Cluster:
+    def __init__(self, _id, vms, provider):
+        self._id = _id
+        self.vms = vms
+        self.provider = BaseProvider.get(provider)
+
+    def create(self):
+        self.cluster = self.provider.create_cluster(self.vms)
+
+        # Store the created cluster inside database
+        cluster_data = {
+            "_id": self._id,
+            "vms": self.vms,
+            "provider": self.provider.name
+            }
+
+        db.clusters.insert(cluster_data)
+
+    def add_worker(self, vm_id):
+        pass
+
+    def add_manager(self, vm_id):
+        pass
+
+    def delete(self):
+        self.provider.delete_server(self._id)
